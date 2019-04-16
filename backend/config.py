77cb27039e785e1274e53f833a34ea7f9a1992f2
clarammdantas <clara.moraesd@gmail.com>
2019-04-16 15:11:46 -0300

    Set project structure

---
+++ backend/config.py
@@ -0,0 +1,31 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+	DEBUG = False
+	TESTING = False
+	CSRF_ENABLED = True
+	SQLALCHEMY_ECHO = False
+	SQLALCHEMY_TRACK_MODIFICATIONS = True
+	SQLALCHEMY_DATABASE_URI = "postgresql://postgres:postgres456@localhost/music_net"
+	SECRET_KEY = "WIll figure out later"
+
+
+
+class ProductionConfig(Config):
+	DEBUG = False
+
+
+class StagingConfig(Config):
+	DEVELOPMENT = True
+	DEBUG = True
+
+
+class DevelopmentConfig(Config):
+	DEVELOPMENT = True
+	DEBUG = True
+	SQLALCHEMY_DATABASE_URI = "postgresql://postgres:postgres456@localhost/music_net"
+
+
+class TestingConfig(Config):
+	TESTING = True
